43c611933fecf2a02083db19d0faa6cd12e68beb
AnastasiyaPY <anastasiya.h991@gmail.com>
2023-12-20 10:14:01 +0300

    2 commit

--- jwt_project/my_app/urls.py
+++ jwt_project/my_app/urls.py
@@ -1,12 +1,3 @@
-# from django.urls import path
-# from .views import ExampleView
-#
-# urlpatterns = [
-#     path('example/', ExampleView.as_view(), name='example'),
-#
-# ]
-
-
 from django.urls import path
 from .views import HelloView
 
--- jwt_project/my_app/views.py
+++ jwt_project/my_app/views.py
@@ -1,13 +1,3 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# from rest_framework.permissions import IsAuthenticated
-#
-# class ExampleView(APIView):
-#     permission_classes = [IsAuthenticated]
-#
-#     def get(self, request):
-#         return Response({"message": "Authenticated user!"})
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
